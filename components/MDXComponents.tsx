e6d9b5452aa9dbcaed79e462ff0aaaa429b17e84
Ian Chou <iantpe@hotmail.com>
2025-04-26 21:58:31 +0800

    Practice

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -6,6 +6,13 @@ import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 
+export const Warning = ({children}) => (
+  <div className="bg-amber-100 border-l-4 border-amber-500 text-amber-700 p-4 my-4">
+    <p className="font-bold">警告</p>
+    <p>{children}</p>
+  </div>
+);
+
 export const components: MDXComponents = {
   Image,
   TOCInline,
@@ -13,4 +20,5 @@ export const components: MDXComponents = {
   pre: Pre,
   table: TableWrapper,
   BlogNewsletterForm,
+  Warning,
 }
