4e2e7c4739af40064d4cb771f3eb5ce99cce9bfa
Ian Chou <iantpe@hotmail.com>
2025-04-26 22:14:52 +0800

    修正 Warning 組件以符合 Prettier 規範

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -6,12 +6,12 @@ import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 
-export const Warning = ({children}) => (
-  <div className="bg-amber-100 border-l-4 border-amber-500 text-amber-700 p-4 my-4">
+export const Warning = ({ children }) => (
+  <div className="my-4 border-l-4 border-amber-500 bg-amber-100 p-4 text-amber-700">
     <p className="font-bold">警告</p>
     <p>{children}</p>
   </div>
-);
+)
 
 export const components: MDXComponents = {
   Image,
